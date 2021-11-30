cf01828e129a8f704c63f3202d48e7c65ed00916
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-30 13:03:46 -0300

    feat: finalizada as rotas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
-import AlunosController from '../app/controllers/AlunoController';
+import AlunoController from '../app/controllers/AlunoController';
+import CursoController from '../app/controllers/CursoController';
+import CursoAlunoController from '../app/controllers/CursoAlunoController';
 
 const routes = new Router();
 
-routes.get('/alunos', AlunosController.index);
+routes.get('/alunos', AlunoController.index);
+routes.post('/alunos', AlunoController.create);
+routes.get('/alunos/:id', AlunoController.read);
+routes.put('/alunos/:id', AlunoController.update);
+routes.delete('/alunos/:id', AlunoController.delete);
+routes.get('/cursos', CursoController.index);
+routes.post('/cursos/aluno', CursoAlunoController.create);
 
 export default routes;
